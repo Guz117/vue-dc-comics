44b8aed5768c45d58ff2f1ab47e37912b9f61c4f
Guz117 <cristian.guzman117@gmail.com>
2022-01-13 17:04:55 +0100

    header e main

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,21 @@
 <template>
-    <div>ciao</div>
+    <div class="container">
+        <div class="contain">
+        <img src="../assets/img/dc-logo.png" alt="">
+        <ul>
+            <li><a href="">CHARACTERS</a></li>
+            <li><a class="active" href="">COMICS</a></li>
+            <li><a href="">MOVIES</a></li>
+            <li><a href="">TV</a></li>
+            <li><a href="">GAMES</a></li>
+            <li><a href="">COLLECTIBLES</a></li>
+            <li><a href="">VIDEOS</a></li>
+            <li><a href="">FANS</a></li>
+            <li><a href="">NEWS</a></li>
+            <li><a href="">SHOP</a></li>
+        </ul>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -12,5 +28,44 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    .container {
+        width: 100%;
+        height: 100px;
+    }
+    
+    .contain {
+        height: 100px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        width: 70%;
+        margin: 0 auto;
+    }
+
+    img {
+        width: 7%;
+    }
+
+    ul {
+        display: flex;
+        list-style: none;
+        
+       
+         li {
+            text-decoration: none;
+            padding-left: 1em;
+            font-size: 0.7em;
+            & a {
+               text-decoration: none; 
+            }
+        }
+    }
+
+    .active {
+        color: #0282f9;
+        padding-bottom: 94%;
+        border-bottom: 4px solid #0282f9;
+    }
+    
 
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,33 @@
 <template>
-    <div>ciao</div>
+    <div class="container">
+        <div class="contain">
+            <div class="card"></div>
+            <div class="menu">
+                <ul>
+                    <li>
+                        <img src="../assets/img/buy-comics-digital-comics.png" alt=""><span>DIGITAL COMICS
+                        </span>
+                    </li> 
+                    <li>
+                        <img src="../assets/img/buy-comics-merchandise.png" alt="">
+                        <span>DC MERCHANDISE</span>
+                    </li>
+                    <li>
+                        <img src="../assets/img/buy-comics-shop-locator.png" alt="">
+                        <span>SUBSCRIPTION</span>
+                    </li>
+                    <li>
+                        <img src="../assets/img/buy-comics-subscriptions.png" alt="">
+                        <span>COMICS SHOP LOCATOR</span>
+                    </li>
+                    <li>
+                        <img src="../assets/img/buy-dc-power-visa.svg" alt="">
+                        <span>DC POWER VISA</span>
+                    </li>  
+                </ul>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -12,5 +40,43 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    .card {
+        width: 100%;
+        height: 100px;
+        background-color: black;
+    }
 
+    .menu {
+        height: 100%;
+        height: 150px;
+        background-color: #0282f9;
+        display: flex;
+        align-items: center;
+        
+    }
+
+    ul {
+        display: flex;
+        list-style: none;
+        width: 70%;
+        margin: 0 auto;
+        justify-content: space-between;
+        
+    }
+
+    li {
+        display: flex;
+        align-items: center;
+        color: white;
+    }
+
+    span {
+        font-size: 0.9em;
+        padding-left: 0.5em;
+    }
+
+    img {
+        padding-left: 1em;
+        width: 50px;
+    }
 </style>
